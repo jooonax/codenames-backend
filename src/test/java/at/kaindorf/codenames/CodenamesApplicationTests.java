1df075e67d0e3d6ded5cc38665b08b174d527755
jooonax <94799391+jooonax@users.noreply.github.com>
2024-03-13 10:47:03 +0100

    initial commit

---
+++ src/test/java/at/kaindorf/codenames/CodenamesApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.codenames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
