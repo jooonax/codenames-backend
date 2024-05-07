646ec71ea5d76db69347e92632123bec25536263
Rapha <rapha.bader@gmail.com>
2024-05-07 14:19:55 +0200

    User roomcode mapping

---
+++ src/main/java/at/kaindorf/codenames/pojos/User.java
@@ -0,0 +1,21 @@
+package at.kaindorf.codenames.pojos;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Project: codenames-backend
+ * Created by: raph
+ * Date: 2024-05-07
+ * Time: 14:03:12
+ */
+
+@Data
+public class UserJoin {
+
+    private String userName;
+    private String roomCode;
+    private List<String> receiverNames;
+
+}
